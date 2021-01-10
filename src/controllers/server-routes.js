d48e21883a4458103543522f577f2fad8e910326
justin <justin_yih@hotmail.com>
2021-01-10 10:06:11 -0800

    merge

--- src/controllers/server-routes.js
+++ src/controllers/server-routes.js
@@ -6,11 +6,7 @@ const serverUrl = process.env.REACT_APP_WEBSERVER_URL
 const createRestaurant = async (restaurant) => {
     try {
         console.log(restaurant)
-<<<<<<< HEAD
         const result = await axios.post(`${serverUrl}/api/restaurant/create`, restaurant)
-=======
-        const result = await axios.post(`${process.env.REACT_APP_WEBSERVER_URL}/api/restaurant/create`, restaurant)
->>>>>>> 994c4a3768ca26aeb91ce4d8a54c2f59e7529f2c
         const rawData = result.data
         //const qrCodeArray = rawData.data.tables
         const newRestaurantObj = {restaurant_id: rawData.generatedId, restaurant_name: rawData.name, table_qr_codes: rawData.tables, menu: rawData.menu, }
@@ -23,7 +19,7 @@ const createRestaurant = async (restaurant) => {
 
 const getRestaurantByID = async (restaurant_id) => {
     try {
-        const result = await axios.get(`${process.env.REACT_APP_WEBSERVER_URL}/api/restaurant/${restaurant_id}`)
+        const result = await axios.get(`${serverUrl}/api/restaurant/${restaurant_id}`)
         console.log(result.data)
         return result.data
     } catch (err) {
@@ -40,7 +36,7 @@ const getRestaurantByID = async (restaurant_id) => {
 // }
 const placeOrder = async (order) => {
     try {
-        const result = await axios.post(`${process.env.REACT_APP_WEBSERVER_URL}/api/restaurant/${order.restaurant_id}/${order.table_id}/place-order`, {items: order.items})
+        const result = await axios.post(`${serverUrl}/api/restaurant/${order.restaurant_id}/${order.table_id}/place-order`, {items: order.items})
         console.log(result.data) //{orderId: "lJUsrQy8uKbY9Tn1iMf8"}
         return result.data
     } catch (err) {
@@ -51,7 +47,7 @@ const placeOrder = async (order) => {
 
 const getStripeCode = async (orderId) => {
     try {
-        const result = await axios.post(`${process.env.REACT_APP_WEBSERVER_URL}/api/purchase/`, {orderId})
+        const result = await axios.post(`${serverUrl}/api/purchase/`, {orderId})
         console.log(result.data)
         return result.data
     } catch (err) {
@@ -62,7 +58,7 @@ const getStripeCode = async (orderId) => {
 
 const purchaseCompleted = async (orderId) => {
     try {
-        const result = await axios.post(`${process.env.REACT_APP_WEBSERVER_URL}/api/purchase/completed`, {orderId , isPaid : true})
+        const result = await axios.post(`${serverUrl}/api/purchase/completed`, {orderId , isPaid : true})
     } catch (err) {
         handleError(err)
     }
