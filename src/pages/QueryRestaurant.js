3a8ea595bf724b612b28212822495758ac3b2b3b
justin <justin_yih@hotmail.com>
2021-01-09 22:16:41 -0800

    render restaurant data

--- src/pages/QueryRestaurant.js
+++ src/pages/QueryRestaurant.js
@@ -6,14 +6,28 @@ import NavBar from '../components/NavBar'
 
 import { PageContainer, SectionContainer, FormContainer, MainHeading, SubHeading, PrimaryButton, NavbarButton, TextInput, PasswordInput } from '../css/main'
 
+const SearchBar = styled.div`
+display: flex;
+flex-direction: row;
+width: 100%;
+justify-content: center;
+align-items: center;
+background: ${props => props.theme.backgroundColour};
+margin: 2rem;
+border-radius: 25px;
+`
+
 function RestaurantSearch({}) {
 
+    const [restaurants, setRestaurants] = useState({data: [], loading: true})
     const [queryParam, setQueryParam] = useState({query: ""})
     const { query } = queryParam
     const updateInput = e => setQueryParam({ ...query, [e.target.name]: e.target.value });
 
     const queryRestaurant = async (query) => {
+        setRestaurants({data: [], loading: true})
         const result = await SearchRestaurant(query)
+        setRestaurants({loading: false, data: result})
         console.log(result)
     }
 
@@ -26,8 +40,18 @@ function RestaurantSearch({}) {
         <PageContainer>
             <main>
                 <SectionContainer>
-                    <TextInput type="text" placeholder={" name or phonenumber"} name="query" onChange={updateInput}></TextInput>
-                    <PrimaryButton className="Primary-Button" onClick={() => queryRestaurant(query)}>Confirm</PrimaryButton>
+                    <SearchBar>
+                        <TextInput type="text" placeholder={" name or phonenumber"} name="query" onChange={updateInput}></TextInput>
+                        <PrimaryButton className="Primary-Button Search-Bar-Button" onClick={() => queryRestaurant(query)}>Confirm</PrimaryButton>
+                    </SearchBar>
+                    <div>
+                        {!restaurants.loading && Array.isArray(restaurants.data) && restaurants.data.length > 1 && restaurants.data.map((restaurant, index) => 
+                        <div key={index}>
+                            <p>{restaurant.restaurant_name}</p>
+                            <p>{restaurant.restaurant_website}</p>
+                        </div>
+                        )}
+                    </div>
                 </SectionContainer>
             </main>
             <footer>
